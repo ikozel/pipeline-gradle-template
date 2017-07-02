283853631c5e7a50ad915161b5653a01e5c7aabe
Igor Kozel <igorkozel@Igors-MacBook-Pro.local>
2017-07-02 12:20:24 -0700

    pipeline

---
+++ jenkinsfile/pipeline.groovy
@@ -0,0 +1,14 @@
+node {
+        stage('Checkout') {
+                checkout scm
+        }
+        stage('Build') {
+                sh '~/workspace/apache-maven-3.5.0/bin/mvn clean build'
+        }
+        stage('Test') {
+                sh '~/workspace/apache-maven-3.5.0/bin/mvn test'
+        }
+        stage('Package') {
+                sh '~/workspace/apache-maven-3.5.0/bin/mvn jar'
+        }
+}
