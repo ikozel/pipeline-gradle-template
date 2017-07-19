0462e897241ffcaada16d861c0c3034d150100a5
Igor Kozel <igor_kozel@yahoo.com>
2017-07-19 16:15:02 -0700

    Update pipeline.groovy

--- jenkinsfile/pipeline.groovy
+++ jenkinsfile/pipeline.groovy
@@ -2,17 +2,17 @@
 import com.sample.pipeline.ComponentPipeline
 new ComponentPipeline(this).main()
 
-node {
-        stage('Checkout') {
-                checkout scm
-        }
-        stage('Build') {
-                sh 'gradle clean build'
-        }
-        stage('Test') {
-                sh 'gradle test'
-        }
-        stage('Package') {
-                sh 'gradle jar'
-        }
-}
+//node {
+//        stage('Checkout') {
+//                checkout scm
+//        }
+//        stage('Build') {
+//                sh 'gradle clean build'
+//        }
+//        stage('Test') {
+//                sh 'gradle test'
+//        }
+//        stage('Package') {
+//                sh 'gradle jar'
+//        }
+//}
